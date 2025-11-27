047a48135850acfb8b15b2d19e15d7ac0e3a4d60
abigailcbarrion <abigailcbarrion@iskolarngbayan.pup.edu.ph>
2025-11-27 11:47:42 +0800

    separated css and json data, added looping on tech stack and activities

---
+++ data.js
@@ -0,0 +1,33 @@
+var activities = [
+    {
+        "title": "Garmin Lily 2 Active - Showcase",
+        "description": "This webpage showcases Garmin’s elegant fitness smartwatch Lily 2 Active. It shows the features, specifications and variations of the product.",
+        "link": "https://abigailcbarrion.github.io/Garmin-Lily-2-Active/"
+    },
+    {
+        "title": "Italy - Open to Wonder",
+        "description": "This website highlights Italy’s rich culture and scenic beauty. It shows some of the most popular Italian destinations, cultural heritage, and the lifestyle of the locals.",
+        "link": "https://abigailcbarrion.github.io/Italy-Open-to-wonder/index.html"
+    },
+    {
+        "title": "Gallery - Iconic Directors of Modern Cinema and their Works",
+        "description": "A website that lists down some of the most iconic directors of modern cinema and their works. It also includes my favorite movies that I found beautifully crafted.",
+        "link": "https://abigailcbarrion.github.io/Gallery/index.html"
+    }
+];
+
+
+var techstack = {
+    it: [
+        { icon: "devicon-python-plain colored", name: "Python" },
+        { icon: "devicon-java-plain colored", name: "Java" },
+        { icon: "devicon-html5-plain colored", name: "HTML5" },
+        { icon: "devicon-css3-plain colored", name: "CSS3" }
+    ],
+    design: [
+        { image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Adobe_InDesign_CC_icon.svg", name: "InDesign" },
+        { image: "https://upload.wikimedia.org/wikipedia/commons/0/0f/Adobe_Animate_CC_icon_%282020%29.svg", name: "Animate" },
+        { icon: "devicon-photoshop-plain colored", name: "Photoshop" },
+        { icon: "devicon-figma-plain colored", name: "Figma" }
+    ]
+};
